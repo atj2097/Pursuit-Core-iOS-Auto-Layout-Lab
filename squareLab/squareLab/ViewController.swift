fe4fa84a918233084b0bd4114a1524a06a824d26
Adam <adamjackson@pursuit.org>
2019-08-07 14:35:55 -0400

    Number ONe

---
+++ squareLab/squareLab/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  squareLab
+//
+//  Created by God on 8/7/19.
+//  Copyright © 2019 God. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var tealView: UIView!
+    
+    @IBOutlet weak var purpleView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
